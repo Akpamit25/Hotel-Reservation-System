ee1db81d400ccadb34783f93711602aa436911a4
Amit Kumar <amitnitsri25@gmail.com>
2020-10-14 23:46:32 +0530

    UC2 : Ability To Find Cheapest Hotel For A Given Date Range

--- src/com/Capg/Hotel/Hotel.java
+++ src/com/Capg/Hotel/Hotel.java
@@ -25,4 +25,4 @@ public class Hotel {
 	public int getRegularRate() {
 		return regularRate;
 	}
-}
+}//
--- src/com/Capg/Hotel/HotelReservation.java
+++ src/com/Capg/Hotel/HotelReservation.java
@@ -1,5 +1,6 @@
 package com.Capg.Hotel;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +8,34 @@ public class HotelReservation {
 
 	public static List<Hotel> hotelList = new ArrayList<Hotel>();
 
-	public static void addRegularRateAndHotelName(String hotelName, int regularRate) {
+	public static void addRegulaRateAndHotelName(String hotelName, int regularRate) {
 		Hotel hotel = new Hotel(hotelName, regularRate);
 		hotelList.add(hotel);
 	}
 
+	public static void findCheapestHotel(String startDate, String endDate) {
+		LocalDate start = LocalDate.parse(startDate);
+		LocalDate end = LocalDate.parse(endDate);
+
+		Hotel cheapestHotel = null;
+		int minRate = 1000000000;
+
+		for (Hotel hotel : hotelList) {
+			LocalDate tempStart = start;
+			LocalDate tempEnd = end.plusDays(1);
+			int hotelRent = 0;
+			while (!(tempStart.equals(tempEnd))) {
+				hotelRent = hotelRent + hotel.getRegularRate();
+				tempStart = tempStart.plusDays(1);
+			}
+			if (hotelRent < minRate) {
+				minRate = hotelRent;
+				cheapestHotel = hotel;
+			}
+
+		}
+		System.out.println("The Cheapest available option is: ");
+		System.out.println(cheapestHotel.getHotelName() + ", total rent :- " + minRate);
+
+	}
 }
