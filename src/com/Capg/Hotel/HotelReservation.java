10e211288d524a11ff0c6f8e88ad29d60aae5085
Amit Kumar <amitnitsri25@gmail.com>
2020-10-14 21:36:34 +0530

    Welcome To Hotel Reservation System

---
+++ src/com/Capg/Hotel/HotelReservation.java
@@ -0,0 +1,12 @@
+package com.Capg.Hotel;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Welcome To Hotel Reservation Program !! ");
+
+	}
+
+}
